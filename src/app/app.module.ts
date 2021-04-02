1e7224e061fa42c49dad500db8954283a992409b
Amir Dahal <68531462+amirdahal@users.noreply.github.com>
2021-04-02 19:31:58 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './navs/header/header.component';
+import {Route, RouterModule} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FaqComponent } from './faq/faq.component';
+import { TalktousComponent } from './talktous/talktous.component';
+
+const routes: Route[] = [
+  { path: '', component: HomeComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'talk', component: TalktousComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FaqComponent,
+    TalktousComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/talktous/talktous.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-talktous',
+  templateUrl: './talktous.component.html',
+  styleUrls: ['./talktous.component.css']
+})
+export class TalktousComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
